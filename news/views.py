04d8fc8eaccfaf5a3ffbd30b0462e0d46ec7eddb
Arpan Duari <97849574+arpanduari@users.noreply.github.com>
2023-11-05 23:40:57 +0530

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+import os
+import requests
+
+API_KEY = os.environ.get("NEWS_API")
+BASE_URL = f"https://newsapi.org/v2/top-headlines?apikey={API_KEY}&country=in"
+BASE_URL_E = f"https://newsapi.org/v2/everything?apikey={API_KEY}"
+
+
+# Create your views here.
+def index(request):
+    top_news = requests.get(f"{BASE_URL}").json()
+    return render(request, "news/index.html", context={"newses": top_news["articles"]})
+
+
+def category_news(request, category):
+    print(category)
+    top_news = requests.get(f"{BASE_URL}&category={category}").json()
+    return render(
+        request,
+        "news/category_news.html",
+        context={"newses": top_news["articles"], "category": category},
+    )
+
+
+def search_result(request):
+    if request.method == "POST":
+        search_query = request.POST.get("search_query")
+        if search_query is not None and search_query != "":
+            all_news = requests.get(f"{BASE_URL_E}&q={search_query}").json()
+            return render(
+                request,
+                "news/query.html",
+                context={"newses": all_news["articles"], "q": search_query},
+            )
+        else:
+            return redirect("home")
